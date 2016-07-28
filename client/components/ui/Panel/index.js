09bd7240ff312d75ca375add148aedbf2899ca06
Malte W <wessel@rheinschafe.de>
2016-07-28 10:29:11 +0200

    improved panel component

--- client/components/ui/Panel/index.js
+++ client/components/ui/Panel/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { createClass, PropTypes } from 'react';
 import cn from 'classnames';
 
 import Icon from 'components/ui/Icon';
@@ -10,56 +10,65 @@ const scrollbarsStyle = {
     height: 'auto'
 };
 
-const Panel = props => {
-    const {
-        title,
-        icon,
-        scrollable,
-        children,
-        className,
-        bodyClassName,
-        ...rest
-    } = props;
+const Panel = createClass({
 
-    const body = (
-        <div className={cn(styles.body, bodyClassName)}>
-            {children}
-        </div>
-    );
+    displayName: 'Panel',
 
-    return (
-        <div
-            className={cn(
-                styles.container,
-                scrollable && styles.scrollable,
-                className
-            )}
-            {...rest}>
-            {title &&
-                <div className={styles.title}>
-                    {icon && <Icon icon={icon} padded/>}
-                    {title}
-                </div>
-            }
-            {scrollable
-                ? <Scrollbars
-                    style={scrollbarsStyle}
-                    className={styles.scrollbars}>
-                    {body}
-                </Scrollbars>
-                : body
-            }
-        </div>
-    );
-};
+    propTypes: {
+        title: PropTypes.node,
+        icon: PropTypes.string,
+        scrollable: PropTypes.bool,
+        children: PropTypes.node,
+        className: PropTypes.string,
+        bodyClassName: PropTypes.string
+    },
 
-Panel.propTypes = {
-    title: PropTypes.node,
-    icon: PropTypes.string,
-    scrollable: PropTypes.bool,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    bodyClassName: PropTypes.string
-};
+    renderBody(props = {}) {
+        const { bodyClassName } = this.props;
+        return (
+            <div className={cn(styles.body, bodyClassName)} {...props}/>
+        );
+    },
+
+    render() {
+        /* eslint-disable no-unused-vars */
+        const {
+            title,
+            icon,
+            scrollable,
+            children,
+            className,
+            bodyClassName,
+            ...props
+        } = this.props;
+        /* eslint-enable */
+
+        return (
+            <div
+                className={cn(
+                    styles.container,
+                    scrollable && styles.scrollable,
+                    className
+                )}
+                {...props}>
+                {title &&
+                    <div className={styles.title}>
+                        {icon && <Icon icon={icon} padded/>}
+                        {title}
+                    </div>
+                }
+                {scrollable
+                    ? <Scrollbars
+                        style={scrollbarsStyle}
+                        renderView={this.renderBody}
+                        className={styles.scrollbars}>
+                        {children}
+                    </Scrollbars>
+                    : this.renderBody({ children })
+                }
+            </div>
+        );
+    }
+});
 
 export default Panel;
