b2c117102859bc288ef13dbb206ebec71b146237
Malte W <wessel@rheinschafe.de>
2016-07-31 09:42:49 +0200

    improved select component

--- client/components/ui/Select/index.js
+++ client/components/ui/Select/index.js
@@ -24,6 +24,7 @@ export default createClass({
         ]).isRequired,
         options: React.PropTypes.arrayOf(React.PropTypes.object),
         onChange: PropTypes.func.isRequired,
+        block: PropTypes.bool,
         className: PropTypes.string,
     },
 
@@ -33,18 +34,26 @@ export default createClass({
             value,
             options,
             onChange,
+            block,
             className,
             ...props
         } = this.props;
         /* eslint-enable */
 
         return (
-            <div className={cn(styles.container, className)}>
-                <div className={styles.label}>
-                    {getLabel(options, value)}
-                </div>
-                <div className={styles.arrow}>
-                    <Icon icon="chevron-down"/>
+            <div
+                className={cn(
+                    styles.container,
+                    block && styles.block,
+                    className
+                )}>
+                <div className={styles.inner}>
+                    <div className={styles.label}>
+                        {getLabel(options, value)}
+                    </div>
+                    <div className={styles.arrow}>
+                        <Icon icon="chevron-down"/>
+                    </div>
                 </div>
                 <select
                     value={value}
