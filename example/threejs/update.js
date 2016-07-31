a4f21f1cf2032592dd614e8f4c58b3b366a2b184
Malte W <wessel@rheinschafe.de>
2016-07-31 15:36:20 +0200

    updated threejs example

--- example/threejs/update.js
+++ example/threejs/update.js
@@ -1,5 +1,7 @@
-export default function update(canvas, data) {
-    const { mesh, scene, camera, renderer } = data;
+export default function update(canvas, data, values) {
+    const { color } = values;
+    const { material, mesh, scene, camera, renderer } = data;
+    material.color.setHex(color.replace('#', '0x'));
     mesh.rotation.x += 0.005;
     mesh.rotation.y += 0.005;
     renderer.render(scene, camera);
