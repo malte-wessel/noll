532ee18ffb5811306f8bede3ec9cbf5e9a32134e
Malte W <wessel@rheinschafe.de>
2016-07-31 11:07:58 +0200

    added select control

---
+++ client/components/app/Experiment/Drawer/Controls/SelectControl/index.js
@@ -0,0 +1,42 @@
+import cn from 'classnames';
+import React, { PropTypes } from 'react';
+import Select from 'components/ui/Select';
+
+import enhance from './enhance';
+import styles from './styles.scss';
+
+const SelectControl = props => {
+    const { control, value, onChange, className } = props;
+    const { label, options } = control;
+    return (
+        <div className={cn(styles.control, className)}>
+            <div className={styles.label}>{label}</div>
+            <div className={styles.input}>
+                <Select
+                    value={value}
+                    options={options}
+                    onChange={onChange}
+                    block/>
+            </div>
+        </div>
+    );
+};
+
+const stringOrNumberType = PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+]);
+
+SelectControl.propTypes = {
+    control: PropTypes.object.isRequired,
+    value: stringOrNumberType.isRequired,
+    options: PropTypes.arrayOf(PropTypes.shape({
+        value: stringOrNumberType.isRequired,
+        label: stringOrNumberType.isRequired
+    })),
+    onChange: PropTypes.func.isRequired,
+    className: PropTypes.string
+};
+
+
+export default enhance(SelectControl);
